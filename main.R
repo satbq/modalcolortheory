168d98ad111d0ef9e2e8fdd14c542b843c5d8808
satbq <120515622+satbq@users.noreply.github.com>
2023-09-08 21:40:32 -0600

    SCL
    Added functions to read & write to Scala files

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 globaledo <- 12
-tiny <- 1e-12
+tiny <- 1e-10
 globalrounder <- -log(tiny)/log(10)
 fortenums <- readRDS("fortenums.rds")
 ineqmats <- readRDS("ineqmats.rds")
@@ -729,4 +729,128 @@ colornum <- function(set, ineqmat=NULL, edo=globaledo, rounder=globalrounder,
   signvec <- toString(signvector(set, ineqmat, edo, rounder))
 
   return(which(signvector_list[[card]]==signvec))
+}
+
+writeSCL <- function(x, filename, period=2, ineqmat=NULL, edo=globaledo, rounder=globalrounder) {
+  # Period defined as a frequency ratio (i.e. 2 for octave-repeading scales)
+  periodCents <- 1200 * log(period)/log(2)
+
+  if (missing(filename)) {
+    filename <- deparse(substitute(x))
+    filename <- paste0(filename, ".scl")
+  }
+
+  ###Comment with the filename
+
+  line0 <- paste0("! ",filename)
+
+  ###The scale description
+  card <- length(x)
+  freedom <- howfree(x)
+  svzeroes <- countsvzeroes(x, ineqmat=ineqmat, edo=edo, rounder=rounder)
+  scaleratio <- round(ratio(x, edo=edo, rounder=rounder),3)
+  how_even <- round(evenness(x, edo=edo), 3)
+
+  nameColor <- FALSE
+  if (exists(deparse(substitute(representative_signvectors)))) {
+    color <- colornum(x,ineqmat=ineqmat, edo=edo, rounder=rounder,
+                     signvector_list=representative_signvectors)
+    nameColor <- TRUE
+  }
+
+
+  if (freedom==1) { degree <- "degree" } else { degree <- "degrees" }
+  if (svzeroes==1) { zeroes <- "hyperplane" } else { zeroes <- "hyperplanes" }
+
+  if (nameColor==TRUE) {
+    line1 <- paste0("A ", card, "-note scale of color ", color, " with ", freedom, " ", degree, " of freedom. Period: ", round(periodCents,3), " cents. Ratio: ", scaleratio, "; evenness: ", how_even, " (wrt. ", edo, " steps to period); on ", svzeroes, " ", zeroes, ".")
+  } else {
+    line1 <- paste0("A ", card, "-note scale with ", freedom, " ", degree, " of freedom. Period: ", round(periodCents,3), " cents. Ratio: ", scaleratio, "; evenness: ", how_even, " (wrt. ", edo, " steps to period); on ", svzeroes, " ", zeroes, ".")
+  }
+
+  if (how_even < 10^-rounder) { line1 <- paste0("A ", card, "-note equal division of ", round(periodCents, 3), " cents.") }
+
+  ###The data for Scala
+
+  line2 <- paste(card)
+  line3 <- "! "
+  x <- c(x,edo)
+  scaledef <- convert(x,edo,periodCents)[-1]
+  scaledef <- format(scaledef,nsmall=1,digits=(rounder+2))
+
+  fileConn <- file(filename)
+  writeLines(c(line0,line1,line2,line3,scaledef),fileConn)
+  close(fileConn)
+}
+
+
+readSCL <- function(filename, scaleonly=TRUE, edo=globaledo) {
+#Note that if scala files aren't defined with enough precision, they may appear to lack structure.
+#Check how close they are to hyperplanes to adjust rounder and tiny appropriately.
+  contents <- scan(filename,what="character",sep="\n",quiet=TRUE,blank.lines.skip=FALSE)
+
+  removeAfterChar <- function(string,charToRemove) {
+    charindex <- as.vector(regexpr(charToRemove,string,fixed=TRUE))
+    charindex[charindex ==-1] <- nchar(string[charindex==-1]) + 1
+    charindex <- charindex - 1
+    res <- do.call(substr,list(x=string,start=rep(0,length(string)),stop=charindex))
+    res <- res[res!=""]
+    return(res)
+  }
+
+  #Fill in description line if blank
+  bangindex <- as.vector(grepl("!",contents,fixed=TRUE))
+  firstrealline <- contents[which(bangindex==FALSE)[1]]
+  if (firstrealline == "") { contents[which(bangindex==FALSE)[1]] <- "blank description" }
+
+  #Remove comments marked by !
+  contents <- removeAfterChar(contents,"!")
+  contents <- trimws(contents)
+
+  #Read scale length & remove description header
+  card <- strtoi(contents[2])
+  if (class(card) != "integer") { warning(".scl file not formatted as expected. Scale length not found.") }
+  contents <- contents[-(1:2)]
+
+  #Locate the degrees defined as ratios vs. those defined by cents
+  ratioindex <- grepl("/",contents,fixed=TRUE)
+  centsindex <- grepl(".",contents,fixed=TRUE)
+
+  #Remove whitespace-prefixed comments
+  contents <- trimws(contents)
+  contents <- removeAfterChar(contents," ")
+
+  #Check for integers (e.g. an octave defined as "2" rather than "2/1")
+  integerindex <- !is.na(strtoi(contents))
+
+  if (sum(ratioindex,centsindex,integerindex) != card) { warning(".scl file not formatted as expected. Cents or ratios incorrectly identified.") }
+
+  #Convert the ratios to scale degrees
+  ratioToLog <- function(ratioString, edo=edo) {
+    dividends <- unlist(strsplit(ratioString, split="/"))
+    if ( length(dividends) != 2 ) { warning(".scl file not formatted as expected. Apparent ratio with more or less than 2 arguments.") }
+    dividends <- as.numeric(dividends)
+    res <- dividends[1]/dividends[2]
+    res <- edo * log(res)/log(2)
+    return(res)
+  }
+
+  #Convert integers to scale degrees
+  intToLog <- function(intString, edo=edo) {
+    val <- strtoi(intString)
+    return(edo * log(val)/log(2))
+  }
+
+  #Apply above functions and convert cents to global EDO
+  if (sum(ratioindex) > 0) { contents[ratioindex] <- sapply(contents[ratioindex],ratioToLog, edo=edo) }
+  if (sum(integerindex) > 0) {contents[integerindex] <- sapply(contents[integerindex],intToLog) }
+  if (sum(centsindex) > 0) { contents[centsindex] <- sapply(contents[centsindex],as.numeric) * (edo/1200) }
+  contents <- as.numeric(contents)
+
+  #Format & output results
+  names(contents) <- NULL
+  octave <- contents[length(contents)]
+  contents <- c(0,contents[-length(contents)])
+  if (scaleonly==TRUE) { return(contents) }
+  return(list(scale=contents,length=card,period=octave))
 }
